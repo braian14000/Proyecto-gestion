2d4d09077a3310083e31e3d6274cc26a219ec55a
Brraian FG <163322613+braian14000@users.noreply.github.com>
2026-06-23 11:50:19 -0300

    Update ModeUsers.py

--- Flask-login/src/models/ModeUsers.py
+++ Flask-login/src/models/ModeUsers.py
@@ -1,30 +1,35 @@
-from .entities.users import User              
+from .entities.users import User
+
 class ModelUser():
 
     @classmethod
-    def login(self, db, user):
+    def login(cls, db, user):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT id, username, password, fullname FROM user WHERE username = %s"
-            cursor.execute(sql, (user.username,))
+            # Buscamos por email y seleccionamos las columnas correctas
+            sql = "SELECT id, username, email, password, telefono FROM user WHERE email = %s"
+            cursor.execute(sql, (user.email,))
             row = cursor.fetchone()
+            
             if row != None:
-                user = User(row[0], row[1], User.check_password(row[2], user.password), row[3])
-                return user
+                # Mapeamos los datos respetando el constructor de User
+                user_match = User(row[0], row[1], row[2], User.check_password(row[3], user.password), row[4])
+                return user_match
             else:
                 return None
         except Exception as ex:
             raise Exception(ex)
 
     @classmethod
-    def get_by_id(self, db, id):
+    def get_by_id(cls, db, id):
         try:
             cursor = db.connection.cursor()
-            sql = "SELECT id, username, fullname FROM user WHERE id = %s"
+            sql = "SELECT id, username, email, telefono FROM user WHERE id = %s"
             cursor.execute(sql, (id,))
             row = cursor.fetchone()
+            
             if row != None:
-                return User(row[0], row[1], None, row[2])
+                return User(row[0], row[1], row[2], None, row[3])
             else:
                 return None
         except Exception as ex:
